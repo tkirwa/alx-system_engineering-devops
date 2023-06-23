e33f8839e2896e6e7fa96c31ee9b1318d68894ca
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-23 13:59:23 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-# Execute a command to kill a process named "killmenow"
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',  # Specify the command to execute using pkill to kill the process
-  path        => '/usr/bin',            # Set the path to the location of the pkill command
-  refreshonly => true,                  # Ensure the exec resource is only triggered when explicitly refreshed
+# Execute a command to kill the process named "killmenow"
+
+# Define an exec resource named 'pkill'
+exec { 'pkill':
+  command  => 'pkill killmenow',  # Specify the command to execute, using pkill to kill the process named "killmenow"
+  provider => 'shell',            # Use the shell as the provider to execute the command
 }

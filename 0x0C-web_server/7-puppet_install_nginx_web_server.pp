5cf06ec45512c1819e699202a882af07097d5cf1
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-27 15:14:59 +0000

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,9 @@
-class nginx {
-  package { 'nginx':  # Install Nginx package
-    ensure => installed,
-  }
+Add comments to the puppet code below - 7-puppet_install_nginx_web_server.pp
 
-  service { 'nginx':  # Ensure Nginx service is running and enabled
-    ensure => running,
-    enable => true,
-  }
 
-  file { '/var/www/html/index.html':  # Create index.html file with "Hello World!" content
-    ensure  => present,
-    content => 'Hello World!',
-    require => Package['nginx'],  # Require Nginx package to be installed
-  }
+# Installs a Nginx server
 
-  file { '/etc/nginx/sites-available/default':  # Configure Nginx server block
-    ensure  => present,
-    content => '
-      server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html;
-
-        location = / {  # Handle root location
-          return 200 "Hello World!";  # Return "Hello World!" with 200 status code
-        }
-
-        location = /redirect_me {  # Handle /redirect_me location
-          return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4/;  # Perform 301 redirect
-        }
-      }
-    ',
-    require => Package['nginx'],  # Require Nginx package to be installed
-    notify  => Service['nginx'],  # Notify Nginx service to restart if the configuration changes
-  }
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/tkirwa permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
-
-include nginx  # Include the nginx class in the manifest
-

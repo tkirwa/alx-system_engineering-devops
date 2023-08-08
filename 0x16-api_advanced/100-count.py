327448fcce30652980599e98ff5335e869e31620
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-08 12:29:14 +0300

    100-count.py - write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,9 +3,12 @@
 
 import requests
 
+
 def count_words(subreddit, word_list, after='', word_dict={}):
-    """ A function that queries the Reddit API, parses the titles of all hot articles,
-    and prints a sorted count of given keywords (case-insensitive, delimited by spaces).
+    """ A function that queries the Reddit API, parses the titles of all
+      hot articles,
+    and prints a sorted count of given keywords (case-insensitive,
+      delimited by spaces).
     'Javascript' should count as 'javascript', but 'java' should not.
     If no posts match or the subreddit is invalid, it prints nothing.
     """
@@ -20,7 +23,8 @@ def count_words(subreddit, word_list, after='', word_dict={}):
     if after is None:
         # Sort the word_dict by count (descending) and then by word (ascending)
         wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-        # Print the sorted count for each keyword with occurrences greater than 0
+        # Print the sorted count for each keyword with occurrences...
+        #  greater than 0
         for word in wordict:
             if word[1]:
                 print('{}: {}'.format(word[0], word[1]))
@@ -30,14 +34,16 @@ def count_words(subreddit, word_list, after='', word_dict={}):
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
     header = {'user-agent': 'redquery'}
     parameters = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=header, params=parameters, allow_redirects=False)
+    response = requests.get(url, headers=header, params=parameters,
+                            allow_redirects=False)
 
     # Check if the API request was successful (status code 200)
     if response.status_code != 200:
         return None
 
     try:
-        # Parse the JSON response to get the list of hot articles and the 'after' parameter
+        # Parse the JSON response to get the list of hot articles and...
+        #  the 'after' parameter
         hot = response.json()['data']['children']
         aft = response.json()['data']['after']
 

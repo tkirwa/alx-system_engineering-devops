ce59478fd2995e2c4371c9e98b26d76aa4b75d47
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-08 12:28:27 +0300

    100-count.py - write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,56 @@
 #!/usr/bin/python3
-"""
-100-count.py
-"""
+""" Module for a function that queries the Reddit API recursively."""
 
 import requests
 
+def count_words(subreddit, word_list, after='', word_dict={}):
+    """ A function that queries the Reddit API, parses the titles of all hot articles,
+    and prints a sorted count of given keywords (case-insensitive, delimited by spaces).
+    'Javascript' should count as 'javascript', but 'java' should not.
+    If no posts match or the subreddit is invalid, it prints nothing.
+    """
 
-def count_words(subreddit, word_list, hot_list=[], after=None):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    # Set a custom User-Agent header
-    headers = {'User-Agent': 'MyRedditApp/1.0'}
-    params = {'after': after} if after else {}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    # Initialize the word_dict if it's empty
+    if not word_dict:
+        for word in word_list:
+            if word.lower() not in word_dict:
+                word_dict[word.lower()] = 0
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        if not posts:
-            return count_occurrences(hot_list, word_list)
-        else:
-            after = data['data']['after']
-            for post in posts:
-                hot_list.append(post['data']['title'].lower())
-            return count_words(subreddit, word_list, hot_list, after)
-    else:
+    # If the 'after' parameter is None, we have reached the end of recursion
+    if after is None:
+        # Sort the word_dict by count (descending) and then by word (ascending)
+        wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+        # Print the sorted count for each keyword with occurrences greater than 0
+        for word in wordict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
         return None
 
+    # Build the URL for the Reddit API request
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    header = {'user-agent': 'redquery'}
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=header, params=parameters, allow_redirects=False)
 
-def count_occurrences(hot_list, word_list, counts={}):
-    if not hot_list:
-        sorted_counts = sorted(counts.items(), key=lambda item: (-item[1],
-                                                                 item[0]))
-        for word, count in sorted_counts:
-            print("{}: {}".format(word, count))
-    else:
-        title = hot_list.pop()
-        for word in word_list:
-            if f" {word.lower()} " in f" {title} ":
-                counts[word.lower()] = counts.get(word.lower(), 0) + 1
-        return count_occurrences(hot_list, word_list, counts)
+    # Check if the API request was successful (status code 200)
+    if response.status_code != 200:
+        return None
 
+    try:
+        # Parse the JSON response to get the list of hot articles and the 'after' parameter
+        hot = response.json()['data']['children']
+        aft = response.json()['data']['after']
 
-if __name__ == "__main__":
-    import sys
+        # Count occurrences of keywords in each article title
+        for post in hot:
+            title = post['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
 
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'"
-              .format(sys.argv[0]))
-    else:
-        subreddit = sys.argv[1]
-        word_list = [x.lower() for x in sys.argv[2].split()]
-        count_words(subreddit, word_list)
+            for word in word_dict.keys():
+                word_dict[word] += lower.count(word)
+
+    except Exception:
+        return None
+
+    # Continue the recursion with the new 'after' parameter
+    count_words(subreddit, word_list, aft, word_dict)

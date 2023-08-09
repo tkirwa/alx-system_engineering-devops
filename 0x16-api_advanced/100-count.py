1b032f23a3f9968af7f0ec7fc87a3bd8819daad5
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-10 00:37:23 +0300

    100-count.py - write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,32 +3,52 @@
 from requests import request
 
 
-def count_words(subreddit, word_list, after="", counter={}, ini=0):
+def count_words(subreddit, word_list, after="", counter=None, ini=0):
     """A recursive function that queries the Reddit API, parses the title
     of all hot articles, and prints a sorted count of given keywords
     (case-insensitive, delimited by spaces. Javascript should count as
     javascript, but java should not)
     """
+    if counter is None:
+        counter = {}
+
     if ini == 0:
         for word in word_list:
             counter[word] = 0
 
-    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
+    url = f"https://api.reddit.com/r/{subreddit}/hot?after={after}"
     headers = {"User-Agent": "Python3"}
     response = request("GET", url, headers=headers).json()
+
     try:
         top = response['data']['children']
         _after = response['data']['after']
         for item in top:
             for word in counter:
-                counter[word] += item['data']['title'].lower(
-                    ).split(' ').count(word.lower())
+                counter[word] += (
+                    item['data']['title'].lower().split(' ').
+                    count(word.lower())
+                    )
         if _after is not None:
             count_words(subreddit, word_list, _after, counter, 1)
         else:
-            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
-            for name, num in str:
+            sorted_counts = sorted(counter.items(), key=lambda kv: (-kv[1],
+                                                                    kv[0]))
+            for name, num in sorted_counts:
                 if num != 0:
                     print('{}: {}'.format(name, num))
     except Exception:
         return None
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'"
+              .format(sys.argv[0]))
+    else:
+        subreddit = sys.argv[1]
+        word_list = [x.lower() for x in sys.argv[2].split()]
+        count_words(subreddit, word_list)

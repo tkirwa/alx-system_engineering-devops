91fc88159359e7e4bdcd7bc2c86a753eb24d194e
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-09 23:59:05 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,7 @@
-# Define a file resource to manage the content of wp-settings.php
-file { '/var/www/html/wp-settings.php':
-  ensure  => present, # Ensure the file exists
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'), # Replace 'phpp' with 'php' in the file content
-  notify  => Exec['restart_apache'], # Notify the restart_apache exec resource if changes are made
-}
+# Puppet code to fix bad 'phpp' extensions to 'php' in the WordPress file 'wp-settings.php'
 
-# Define an exec resource to restart the Apache service
-exec { 'restart_apache':
-  command => 'systemctl restart apache2', # Command to restart the Apache service
-  path    => '/usr/local/bin/:/bin/', # Set the PATH for the command execution
-  refreshonly => true, # Only run this exec resource when notified (when file content changes)
+# Define an exec resource named 'fix-wordpress'
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php', # Use 'sed' to replace 'phpp' with 'php' in the file
+  path    => '/usr/local/bin/:/bin/' # Set the PATH for the command execution
 }

2a167eb603ec4473f583d8eaea951d2d188ac118
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-11 00:40:11 +0300

    100-count.py - write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,52 @@
-#!/usr/bin/python3
-"""A file to make a query to an endpoint"""
-from requests import request
+import requests
 
 
-def count_words(subreddit, word_list, after="", counter=None, ini=0):
-    """A recursive function that queries the Reddit API, parses the title
-    of all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces. Javascript should count as
-    javascript, but java should not)
-    """
-    if counter is None:
-        counter = {}
+# Function to get the hot articles from a subreddit using Reddit API
+def get_hot_articles(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Reddit Keyword Counter"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()["data"]["children"]
+    else:
+        return []
 
-    if ini == 0:
-        for word in word_list:
-            counter[word] = 0
 
-    url = f"https://api.reddit.com/r/{subreddit}/hot?after={after}"
-    headers = {"User-Agent": "Python3"}
-    response = request("GET", url, headers=headers).json()
+# Function to count occurrences of keywords in titles
+def count_keywords_in_titles(titles, keywords):
+    keyword_count = {keyword: 0 for keyword in keywords}
+    for title in titles:
+        title_text = title["data"]["title"].lower()
+        for keyword in keywords:
+            if f" {keyword} " in f" {title_text} ":
+                keyword_count[keyword] += 1
+    return keyword_count
 
-    try:
-        top = response['data']['children']
-        _after = response['data']['after']
-        for item in top:
-            for word in counter:
-                counter[word] += (
-                    item['data']['title'].lower().split(' ').
-                    count(word.lower())
-                    )
-        if _after is not None:
-            count_words(subreddit, word_list, _after, counter, 1)
-        else:
-            sorted_counts = sorted(counter.items(), key=lambda kv: (-kv[1],
-                                                                    kv[0]))
-            for name, num in sorted_counts:
-                if num != 0:
-                    print('{}: {}'.format(name, num))
-    except Exception:
-        return None
+
+# Main function to count and print sorted keyword counts
+def count_words(subreddit, word_list):
+    if not word_list:
+        return
+
+    titles = get_hot_articles(subreddit)
+    keyword_count = count_keywords_in_titles(titles, word_list)
+
+    sorted_keywords = sorted(
+        keyword_count.items(),
+        key=lambda item: (-item[1], item[0])
+    )
+
+    for keyword, count in sorted_keywords:
+        print(f"{keyword}: {count}")
 
 
 if __name__ == "__main__":
     import sys
-
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
         print("Ex: {} programming 'python java javascript'"
               .format(sys.argv[0]))
     else:
         subreddit = sys.argv[1]
-        word_list = [x.lower() for x in sys.argv[2].split()]
-        count_words(subreddit, word_list)
+        keywords = [word.lower() for word in sys.argv[2:]]
+        count_words(subreddit, keywords)

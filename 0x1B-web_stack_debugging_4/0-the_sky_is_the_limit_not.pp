b739eb7cdc454aeaaca282e36a31460ce0fe99c9
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-17 09:20:59 +0300

    0-the_sky_is_the_limit_not.pp fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,13 @@
-# changing limtis
+# Adjusts the file limit for the Nginx process.
 
+# Replace the existing ULIMIT value in the nginx configuration file
 exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart'],
 }
 
+# Restart the Nginx service to apply the changes
 exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
